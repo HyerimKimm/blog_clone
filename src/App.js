b1f6d094a7d684b5ed9e2167103ba24d67319749
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-05-13 19:10:30 +0900

    컴포넌트 만들기, useState 연습

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
+/* esLint-disable */
+
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let [글제목,글제목변경] = useState(['2023 패션 트랜드','서울 이자카야 best10','서울 포케 맛집 best5']);
+  let [따봉, 따봉변경] = useState(0);
 
-
+  function 글제목바꾸기() {
+    let newArray=[];
+    newArray = 글제목.map( e=>{
+      return e.replace('서울','경기');
+    } )
+    console.log(newArray);
+    글제목변경(newArray);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
-        개발 Blog
+        <div>개발 Blog</div>
       </div>
-      <h4>블로그 글</h4>
+      <button onClick={ 글제목바꾸기 }>버튼</button>
+      <div className='list'>
+        <h3>{글제목[0]}</h3>
+        <p>5월 13일 발행</p>
+      </div>
+      <hr/>
+      <div className='list'>
+        <h3>{글제목[1]}<span onClick={()=>{
+          따봉변경(따봉+1);
+         }}>👍🏻</span> {따봉}</h3>
+        <p>5월 13일 발행</p>
+      </div>
+      <hr/>
+      <div className='list'>
+        <h3>{글제목[2]}</h3>
+        <p>5월 13일 발행</p>
+      </div>
+      <hr/>
+      <Modal>
+         
+      </Modal>
     </div>
+  );
+}
+
+function Modal(){
+  return (
+    <div></div>
   );
 }
 
